1936e60a56efe73b38abbbfc6a39a9ae03aad66e
Marco Ippolito <marcoippolito54@gmail.com>
2022-10-05 17:43:34 +0200

    refactor: code cleanup

--- example/plugins/mercurius-explain.js
+++ example/plugins/mercurius-explain.js
@@ -2,7 +2,7 @@ import fp from 'fastify-plugin'
 
 export default fp(
   async fastify => {
-    fastify.register(import('../../lib/index.js'))
+    fastify.register(import('../../lib/index.js'), { enabled: true })
   },
   {
     name: 'mercurius-explain',
