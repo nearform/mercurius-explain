3eab001a3120cf801cd355c27ffb766b6ec5dbbc
Marco Ippolito <marcoippolito54@gmail.com>
2022-10-05 11:58:10 +0200

    feat: created base test case

--- test/basic.test.js
+++ test/basic.test.js
@@ -1,12 +1,50 @@
 import { test } from 'tap'
 import Fastify from 'fastify'
 import mercuriusExplain from '../lib/index.js'
-import mercurius from './shared/mercurius.js'
+import mercurius from 'mercurius'
 
-test('', async t => {
-  const fastify = Fastify()
-  await fastify.register(mercurius)
-  await fastify.register(mercuriusExplain)
-  t.teardown(() => fastify.close())
-  await fastify.listen()
+test('return explain value', async t => {
+  const app = Fastify()
+  t.teardown(app.close.bind(app))
+
+  const schema = `
+    type Query {
+      add(x: Int, y: Int): Int
+      hello: String
+    }
+  `
+
+  const resolvers = {
+    Query: {
+      async add(_, { x, y }) {
+        t.pass('add called only once')
+        return x + y
+      }
+    }
+  }
+
+  app.register(mercurius, {
+    schema,
+    resolvers
+  })
+
+  app.register(mercuriusExplain, {})
+
+  const query = '{ add(x: 2, y: 2) }'
+  const res = await app.inject({
+    method: 'POST',
+    url: '/graphql',
+    body: {
+      query
+    }
+  })
+
+  t.equal(res.statusCode, 200)
+  const { data } = res.json()
+  t.has(data, {
+    add: 4
+  })
+  t.type(data['__explain'], Array)
+  t.equal(data['__explain'].length, 1)
+  t.hasProps(data['__explain'].pop(), ['path', 'begin', 'end', 'time'])
 })
