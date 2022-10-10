612e92895a5296fbe6e67c16d1e87af41b5d9839
Marco Ippolito <marcoippolito54@gmail.com>
2022-10-10 15:19:07 +0200

    fix: changed post autocannon

--- test/benchmark.js
+++ test/benchmark.js
@@ -14,7 +14,11 @@ benchmark()
 
 async function startAutocannon({ enabled }) {
   const app = await startServer({ enabled })
-  const query = `{
+  const instance = await autocannon({
+    url: `http://localhost:3002/graphql`,
+    method: 'POST',
+    body: JSON.stringify({
+      query: `{
         users {
           name
           status {
@@ -25,11 +29,8 @@ async function startAutocannon({ enabled }) {
           }
         }
     }`
-  const instance = await autocannon({
-    url: `http://localhost:3002/graphql?query=${query}`,
-    method: 'GET',
+    }),
     connections: 10,
-    pipelining: 1,
     duration: 10
   })
   app.close.bind(app)
