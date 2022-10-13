6bc3c90faf75cf04d5b4dad4ec97da2cb5ba13a2
Marco Ippolito <marcoippolito54@gmail.com>
2022-10-13 11:03:57 +0200

    feat: added enabled as function and changed structure

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 import fp from 'fastify-plugin'
 import { Collector } from './lib/collector.js'
 import wrapResolvers from './lib/wrapResolvers.js'
+import { DEFAULT_OPTIONS } from './lib/constant.js'
 
-export default fp(async (fastify, options) => {
+export default fp(async (fastify, deafultOptions) => {
+  const options = { ...DEFAULT_OPTIONS, ...deafultOptions }
   if (!options.enabled) return
-
-  fastify.graphql.addHook('preParsing', async (_, __, context) => {
+  const isEnabled =
+    typeof options.enabled === 'function' ? options.enabled : () => true
+  fastify.graphql.addHook('preParsing', async (request, reply, context) => {
+    if (!isEnabled({ request, reply, context })) return
     context.mercuriusExplainCollector = new Collector()
   })
 
   fastify.graphql.addHook('onResolution', async (execution, context) => {
+    if (!isEnabled({ execution, context })) return
     execution.extensions = {
       ...execution.extensions,
       explain: context.mercuriusExplainCollector.export()
