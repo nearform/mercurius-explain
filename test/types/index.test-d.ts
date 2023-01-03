8429f7647c2eb1d568543f26feb99c27182304c2
Andrea Carraro <me@andreacarraro.it>
2023-01-03 16:07:39 +0100

    test: add type tests for explainGraphiQLPlugin

--- test/types/index.test-d.ts
+++ test/types/index.test-d.ts
@@ -1,6 +1,19 @@
-import { expectAssignable } from 'tsd'
-import { MercuriusExplainOptions } from '../../index'
+import { expectAssignable, expectType, expectError } from 'tsd'
+import { MercuriusExplainOptions, explainGraphiQLPlugin } from '../../index'
 const mercuriusExplainOptions = {
   enabled: true
 }
 expectAssignable<MercuriusExplainOptions>(mercuriusExplainOptions)
+
+// explainGraphiQLPlugin
+type ExplainGraphiQLPluginReturnType = {
+  name: string,
+  umdUrl: string,
+  fetcherWrapper: string,
+};
+
+expectType<ExplainGraphiQLPluginReturnType>(explainGraphiQLPlugin())
+expectType<ExplainGraphiQLPluginReturnType>(explainGraphiQLPlugin({}))
+expectType<ExplainGraphiQLPluginReturnType>(explainGraphiQLPlugin({version: 'v1'}))
+// Version should only accept strings
+expectError(explainGraphiQLPlugin({version: 2}))
