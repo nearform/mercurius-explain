e4fc84e7fdec2e482a3c87dafa1862825cdf60b4
Andrea Carraro <toomuchdesign@users.noreply.github.com>
2023-01-04 09:49:07 +0100

    fix: expose explainGraphiQLPlugin types (#91)
    * fix: expose explainGraphiQLPlugin types
    
    * test: add type tests for explainGraphiQLPlugin
    
    * test: add type tests for default export
    
    * refactor: explainGraphiQLPlugin types

--- test/types/index.test-d.ts
+++ test/types/index.test-d.ts
@@ -1,6 +1,25 @@
-import { expectAssignable } from 'tsd'
-import { MercuriusExplainOptions } from '../../index'
+import type { FastifyPluginAsync } from 'fastify'
+import { expectAssignable, expectType, expectError } from 'tsd'
+import explain, {
+  MercuriusExplainOptions,
+  explainGraphiQLPlugin,
+  ExplainGraphiQLPluginReturnType
+} from '../../index'
+
+// MercuriusExplainOptions
 const mercuriusExplainOptions = {
   enabled: true
 }
 expectAssignable<MercuriusExplainOptions>(mercuriusExplainOptions)
+
+// default export
+expectType<FastifyPluginAsync<MercuriusExplainOptions>>(explain)
+
+// explainGraphiQLPlugin
+expectType<ExplainGraphiQLPluginReturnType>(explainGraphiQLPlugin())
+expectType<ExplainGraphiQLPluginReturnType>(explainGraphiQLPlugin({}))
+expectType<ExplainGraphiQLPluginReturnType>(
+  explainGraphiQLPlugin({ version: 'v1' })
+)
+// Version should only accept strings
+expectError(explainGraphiQLPlugin({ version: 2 }))
